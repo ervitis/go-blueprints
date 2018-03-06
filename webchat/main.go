cd9ddf51764e58bcead84774984253cf271e4475
ervitis <vitomarti@gmail.com>
2018-03-06 22:03:15 +0100

    use types template handler and override servehttp

--- webchat/main.go
+++ webchat/main.go
@@ -3,10 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync"
+	"text/template"
+	"path/filepath"
 )
 
+type templateHandler struct {
+	once sync.Once
+	filename string
+	templ *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
 func main() {
-	http.HandleFunc("/", handleMainPage)
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+
 	port := "8080"
 
 	err := http.ListenAndServe(":" + port, nil)
